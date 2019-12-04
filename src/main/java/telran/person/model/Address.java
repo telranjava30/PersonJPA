89dcbe61cb1b9f30a9763a45d110ec70f65212ce
edward <edward@edward>
2019-12-04 21:02:00 +0200

    Added inheritance

---
+++ src/main/java/telran/person/model/Address.java
@@ -0,0 +1,30 @@
+package telran.person.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Builder
+@Embeddable
+public class Address implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String city;
+	String street;
+	int building;
+
+}
